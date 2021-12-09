e6dfa7b356285b7e2694235c215a51bbb0a39537
Tucker Hartland <tucker.hartland@gmail.com>
2021-12-09 15:55:08 -0800

    adding 2D Stokes inversion for Sarah, using iceModel from ucm-ice repository

---
+++ 06_Stokes/iceModel/__init__.py
@@ -0,0 +1,10 @@
+'''
+Created on Feb 26, 2020
+
+@author: uvilla
+'''
+
+from .nonlinearStokesProblem import NonlinearStokesForm, EnergyFunctionalPDEVariationalProblem
+from .misfit import PointwiseProjectedStateObservation
+
+from .manifoldPrior_noemi import ManifoldPrior
